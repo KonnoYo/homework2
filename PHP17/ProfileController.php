a4e4fa7845a84c0d516a6067453ce2b4582919e4
Konno Yo <sphz2729@lagoon.ocn.ne.jp>
2019-05-14 02:37:10 +0000

    PHP17課題

---
+++ PHP17/ProfileController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Profile;
+use App\ProfileHistory;
+
+use Carbon\Carbon;
+
+class ProfileController extends Controller
+{
+    
+    public function add()
+    {
+        
+    return view('admin.profile.create');    
+        
+    }
+    
+    
+    public function create(Request $request)
+    {
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profiles = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+        
+        return redirect('admin/profile/create');
+        
+    }
+    
+    public function edit(Request $request)
+    {
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);    
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+
+    }
+    
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+        
+        $history = new ProfileHistory;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        
+        return redirect('admin/profile');
+        
+    }
+    
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('name', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+            return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+    
+    public function delete(Request $request)
+    {
+        // 該当するNews Modelを取得
+        $profile = Profile::find($request->id);
+        // 削除する
+        $profile->delete();
+        return redirect('admin/profile/');
+    }
+    
+}
